330fb5b610826607002132661efe5d737db55d10
henryjameslau <henry.james.lau@gmail.com>
2018-02-05 09:36:49 +0000

    clearing

---
+++ topojson-conversion.sh
@@ -0,0 +1,17 @@
+# Bulk convert shapefiles to geojson using ogr2ogr
+# For more information, see http://ben.balter.com/2013/06/26/how-to-convert-shapefiles-to-geojson-for-use-on-github/
+
+# Note: Assumes you're in a folder with one or more zip files containing shape files
+# and Outputs as geojson with the crs:84 SRS (for use on GitHub or elsewhere)
+
+#unzip all files in a directory
+#for var in *.zip; do unzip "$var"; done
+
+#convert all shapefiles
+for shapefile in *.geojson; do
+  geo2topo -o topo_$shapefile.json $shapefile
+done
+
+# You'd probably want to `mv *.topojson [path-to-git-repo]/` at this point
+# so you could commit the file to GitHub
+# Happy mapping!
